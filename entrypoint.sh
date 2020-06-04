4f868d66419f215ef16f36511d0e6e35c43b576e
Joe Moretti <jmoretti@users.noreply.github.com>
2020-06-03 21:54:35 -0500

    Update to AZ CLI requires --auth-mode to be set

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,10 +43,10 @@ az account set --subscription ${AZURE_SUBSCRIPTION_ID}
 
 # Enable Static Website
 if [ -z "$AZURE_ERROR_DOCUMENT_NAME" ]; then
-    az storage blob service-properties update --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --static-website --index-document ${AZURE_INDEX_DOCUMENT_NAME}
+    az storage blob service-properties update --auth-mode login --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --static-website --index-document ${AZURE_INDEX_DOCUMENT_NAME}
 else
-    az storage blob service-properties update --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --static-website --404-document ${AZURE_ERROR_DOCUMENT_NAME} --index-document ${AZURE_INDEX_DOCUMENT_NAME}
+    az storage blob service-properties update --auth-mode login --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --static-website --404-document ${AZURE_ERROR_DOCUMENT_NAME} --index-document ${AZURE_INDEX_DOCUMENT_NAME}
 fi
 
 # Upload source to storage
-az storage blob upload-batch -s ${SOURCE_DIR} -d \$web --account-name ${AZURE_STORAGE_ACCOUNT_NAME}
+az storage blob upload-batch -s ${SOURCE_DIR} -d \$web --auth-mode login --account-name ${AZURE_STORAGE_ACCOUNT_NAME}
